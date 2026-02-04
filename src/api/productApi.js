2e5a6602919fc5a1ad26cea0f1090fc041f0a67f
DUYTAN79 <tranduytan1612003@gmail.com>
2026-02-04 09:21:08 +0700

    Delete src directory

--- src/api/productApi.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const BASE_URL = 'https://api.escuelajs.co/api/v1/products';
-
-export const productApi = {
-  getAll: () => axios.get(BASE_URL),
-  create: (data) => axios.post(BASE_URL, data),
-  update: (id, data) => axios.put(`${BASE_URL}/${id}`, data),
-};
